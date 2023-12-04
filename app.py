b2809653a2498226408a7844c05f7599cf575c52
LeonardoTieppo1 <Leonardo.tieppo2@hotmail.com>
2023-12-04 01:13:54 -0300

    projeto_devops_final_2

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.get("/")
 def hello():
-    return jsonify({'message': 'Hello World!'})
+    return jsonify({'message': 'Hello World and Ola Mundo!'})
 
 if __name__ == '__main__':
     app.run()
